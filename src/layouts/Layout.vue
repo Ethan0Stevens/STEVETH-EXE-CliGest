1609096e59603875ce73e3c5798cbda57f7c52f7
Ethan0Stevens <ethan.stevens@divtec.ch>
2023-02-01 13:29:46 +0100

    Ajout de page Accueil et de page clients

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label header>Menu de navigation</q-item-label>
+        <q-item clickable to="/" exact>
+          <q-item-section avatar>
+            <q-icon name="house" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Accueil</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable to="/clients" exact>
+          <q-item-section avatar>
+            <q-icon name="list" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Clients</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ src/pages/PageClients.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page padding>
+    <h3>Liste des clients</h3>
+    <q-list
+      class="rounded-borders"
+      bordered
+      separator
+    >
+      <client/>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageClients',
+  components: {
+    client: require('../components/Client').default
+  }
+})
+</script>
